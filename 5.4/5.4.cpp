69749db553e2cba735cb3ba91d725794c180698e
Компьютер <@DESKTOP-0TR3ASG>
2023-12-02 12:13:45 +0700

    Добавьте файлы проекта.

---
+++ 5.4/5.4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int n;
+	int* arr;
+	cin >> n;
+	arr = new int[n];
+	for (int i = 0; i < n; i++)
+	//заполняем массив
+		cin >> arr[i];
+	for (int i = 0; i < n - 1; i += 2)
+	//меняем местами элементы массива
+		swap(arr[i], arr[i + 1]);
+	for (int i = 0; i < n; i++)
+	//вывод массива
+		cout << arr[i] << " ";
+	cout << endl;
+	delete[] arr;
+	return 0;
+}
